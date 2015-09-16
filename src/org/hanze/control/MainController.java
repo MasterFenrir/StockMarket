a0496e767125a5c1b6c4553f17b9dd1921fc9b28
MasterFenrir <masterfenrir@outlook.com>
2015-09-16 13:34:52 +0200

    GUI basics work.

--- src/org/hanze/control/MainController.java
+++ src/org/hanze/control/MainController.java
@@ -1,16 +1,25 @@
 package org.hanze.control;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
 /**
  * Created by Sander on 9/16/2015.
  */
 public class MainController implements Initializable {
+
+    private static final String[] VIEW_FILES = {"../gui/GraphView.fxml", "../gui/TextView.fxml"};
 
     @FXML
     private TabPane content;
@@ -19,11 +28,42 @@ public class MainController implements Initializable {
 
     }
 
+    /**
+     * Initialize the GUI
+     *
+     * @param location
+     * @param resources
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        content.getTabs();
-        System.out.println("Hello!");
+        List<Observer> observers = new ArrayList<>();
+        Observer obs;
+        for (String e : VIEW_FILES) {
+            obs = tabLoader(e);
+            if (obs != null) observers.add(obs);
+        }
         Builder builder = new Builder();
         builder.build();
+    }
+
+    /**
+     * Load views into tabs and return the observer
+     *
+     * @param view
+     * @return
+     */
+    private Observer tabLoader(String view) {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(view));
+        try {
+            Pane tab = loader.load();
+            StockView cont = loader.<StockView>getController();
+            String name = cont.getViewName();
+            Tab nTab = new Tab(name, tab);
+            content.getTabs().add(nTab);
+            return (Observer) cont;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
---
+++ src/org/hanze/control/StockView.java
@@ -0,0 +1,11 @@
+package org.hanze.control;
+
+/**
+ * Created by Sander on 9/16/2015.
+ * Interface for StockViews
+ */
+public interface StockView {
+
+    String getViewName();
+
+}
