0f862e02e981fc316bf8516790edf907eb9e4893
MasterFenrir <masterfenrir@outlook.com>
2015-09-16 10:58:32 +0200

    Initial commit.

---
+++ src/org/hanze/control/One.java
@@ -0,0 +1,7 @@
+package org.hanze.control;
+
+/**
+ * Created by Sander on 9/16/2015.
+ */
+public class One {
+}
---
+++ src/org/hanze/model/Three.java
@@ -0,0 +1,7 @@
+package org.hanze.model;
+
+/**
+ * Created by Sander on 9/16/2015.
+ */
+public class Three {
+}
