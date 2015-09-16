447386f95e3f2a80802b0205014e9b0641e3b4ed
Wessel Becker <wesselbecker92@gmail.com>
2015-09-16 12:30:04 +0200

    refuctored the stoke out of the stock maker

--- src/org/hanze/model/Stock.java
+++ src/org/hanze/model/Stock.java
@@ -8,17 +8,16 @@ import java.util.Observable;
  *
  * an observable class that represents a stock and that will start changing automatically when you call the run method.
  */
-public class Stock extends Observable implements Runnable{
+public class Stock extends Observable{
     private String name;
     private double price;
-    private boolean stopped = false;
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+        this.notifyObservers();//notify all  the observers
     }
 
     public double getPrice() {
@@ -27,6 +26,7 @@ public class Stock extends Observable implements Runnable{
 
     public void setPrice(double price) {
         this.price = price;
+        this.notifyObservers();//notify all  the observers
     }
 
     Stock(){
@@ -39,32 +39,5 @@ public class Stock extends Observable implements Runnable{
         super();
         this.name = name;
         this. price = price;
-    }
-
-    /**
-     * stops the thread
-     */
-    public synchronized void stopThread(){
-        this.stopped = true;
-    }
-
-    /**
-     * the running method that continiously changes the price
-     */
-    @Override
-    public void run() {
-        while (!stopped) {//while not stopped keep running
-
-            double randNum = (Math.random() * (.06)) - .03;//get a new random number between -0.03 and 0.03
-            DecimalFormat df = new DecimalFormat("#.##");//get a decimal format that rounds to 2 decimals
-            this.price = Double.valueOf(df.format(price + randNum));//make our new price by adding randNum and rounding to 2 decimals
-            this.notifyObservers();//notify all  the observers
-            try {
-                Thread.sleep(2000);//wait 2 seconds
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        //were done
     }
 }
